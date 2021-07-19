2051fb88fb932f85a198265409435638d3bd6c92
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-07-19 12:58:20 +0200

    added js folder

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <?php wp_head(); ?>
 <div class="header-container">
     <div class="header-image"></div>
-    <div class="login-button"></div>
+    <div class="login-button">
+        <?php?>
+    </div>
     <div class="register-button"></div>
 </div>
