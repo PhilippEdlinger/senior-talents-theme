b9952c9104a533fae331a1565ff51d143f1a8e42
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-07-22 14:53:16 +0200

    login form container

--- login.php
+++ login.php
@@ -4,10 +4,6 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/login-style.css'?>">
 </head>
 
-<p> <?php echo get_template_directory_uri() . '/css/login-style.css'?> </p>
-
-<p> Test </p>
-
 <div class="login-form">
 
 </div>
