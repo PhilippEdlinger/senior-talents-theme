45c2e428af49375a6f03e04142c91321a5dfa04d
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-07-22 20:35:09 +0200

    added signup form container

--- login.php
+++ login.php
@@ -1,9 +1,11 @@
 <?php /* Template Name: Login Page */ ?>
 
 <head>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/login-style.css'?>">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/login-style.css'?>">
 </head>
 
+<body>
 <div class="login-form">
 
 </div>
+</body>
---
+++ signup.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: Signup Page */ ?>
+
+<head>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/signup-style.css'?>">
+</head>
+<body>
+    <div id="signup-form">
+    </div>
+</body>
+
+
