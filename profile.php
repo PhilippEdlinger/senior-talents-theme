d06ae0cc4553d9eba1ebbfded2d5d84fc51f4aeb
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-07-23 21:33:35 +0200

    created profile.css

---
+++ profile.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: Profile Page */ ?>
+
+<head>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/profile-style.css'?>">
+</head>
+
+<body>
+    <div id="profile-container">
+
+    </div>
+</body>
