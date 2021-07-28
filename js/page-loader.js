994e13f62b64767f3b0c2a46f071cf63879571e3
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-07-28 15:17:09 +0200

    added load login page function

--- js/page-loader.js
+++ js/page-loader.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', function () {
     let loginButton = document.getElementsByClassName("login-button");
-    loginButton[0].addEventListener("click", load_page);
+    loginButton[0].addEventListener("click", load_login_page);
 })
 
-function load_page(){
-
+function load_login_page(){
+    console.log("TEst")
+    window.location.assign("http://localhost/wordpress/wordpress/index.php/login/");
 }
