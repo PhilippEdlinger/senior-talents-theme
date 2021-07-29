499ad763f01f25f207c05a433bb8f98429dbefd2
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-07-29 09:41:39 +0200

    added admin taskbar

---
+++ admin.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: Manage Page */ ?>
+
+<head>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/admin-style.css'?>">
+</head>
+
+
+<body>
+    <div id="admin-maincontent">
+        <div id="admin-taskbar"></div>    
+    <div>     
+</body>
