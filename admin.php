64cad44ebe2595d06a367e60a43aa83ccf7c1371
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-08-25 17:43:34 +0200

    reading db table

--- admin.php
+++ admin.php
@@ -4,11 +4,38 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/admin-style.css'?>">
 </head>
 
-
 <body>
+    <table border="2" >
+    <tr>
+        <td> Name </td>
+        <td> Age </td> 
+    </tr> 
+    <?php
+        include "dbConnection.php"; 
+        $records = mysqli_query($db,"select * from Test"); 
+        while($data = mysqli_fetch_array($records)){
+    ?>        
+    
+    <tr>
+        <td><?php echo $data['Name']; ?></td>
+        <td><?php echo $data['Age']; ?></td>
+    </tr>	
+
+    <?php
+    }
+    ?>
+       
+    </table>
+
+    <!-- 
     <div id="admin-maincontent">
         <div id="admin-taskbar"></div>
-        <div class="admin-info-buttons" id="admin-user-info"></div>
-        <div class="admin-info-buttons" id="admin-company-info"></div>          
-    <div>     
+        <div id="admin-option-container">
+            <div class="admin-info-buttons admin-user-info" ></div>
+            <div class="admin-info-buttons" id="admin-company-info"></div>          
+        </div> 
+        <div>
+    -->     
 </body>
+
+
