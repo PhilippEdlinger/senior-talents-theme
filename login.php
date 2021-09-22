6d2f955c7b9d8a710e57d896881210eed77a4dee
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-09-22 19:32:25 +0200

    added input fields and button in login

--- login.php
+++ login.php
@@ -2,10 +2,17 @@
 
 <head>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/login-style.css'?>">
+    <script src="<?php echo get_template_directory_uri() . '/js/login.js'?>"></script>
 </head>
 
 <body>
 <div class="login-form">
-
+    <div class="login-form-section">
+            <input placeholder="Email" id="login-form-input-email" type="text"></input>
+    </div>
+    <div class="login-form-section">
+            <input placeholder="Password" id="login-form-input-password" type="password"></input>
+    </div>
+    <button id="login-button">Login</button>
 </div>
 </body>
