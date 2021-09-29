713210494319e9fda9d8314cbde4b079c1b1b764
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-09-29 21:03:28 +0200

    removed admin_bar

--- functions.php
+++ functions.php
@@ -12,4 +12,9 @@ function senior_talents_files() {
 
 add_action('wp_enqueue_scripts', 'senior_talents_files');
 
+function remove_admin_login_header() {
+  remove_action('wp_head', '_admin_bar_bump_cb');
+}
+add_action('get_header', 'remove_admin_login_header');
+
 ?>
