664ba85499403ebdc5b233d6a8b181a2bf1a4b26
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-10-27 19:01:17 +0200

    added redirection

--- dbConnection.php
+++ dbConnection.php
@@ -1,11 +1,6 @@
 <?php /* Template Name: dbconnection */ ?>
 
 <?php
-/*
-$response = wp_remote_get( 'http://localhost:8080/seniors' );
-$body     = wp_remote_retrieve_body($response);
-    return json_decode($body)
-*/
 
 $email = $_POST['login-form-input-email'];
 $pwd = $_POST['login-form-input-password'];
@@ -13,7 +8,12 @@ $pwd = $_POST['login-form-input-password'];
 $http_request = "http://localhost:8080/senior/getByEmailPassword?email=" . $email . "&password=". $pwd;
 
 $response = wp_remote_get($http_request);
-$body     = wp_remote_retrieve_body($response);
-echo $body;
+$body     = wp_remote_retrieve_body($response); 
+ if (substr_count( $body, "\n" ) == 0){
+    header("Location: https://localhost/wordpress/wordpress/index.php/profile");
+ }else{
+    header("Location: https://localhost/wordpress/wordpress/index.php/login");
+ }
+ 
 
 ?>
