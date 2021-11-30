a37770d80af7d539ea1a6e175c5b042dd03c9c1c
Jakob Ratzenböck <gaglimon@gmail.com>
2021-11-30 19:11:39 +0100

    style for profile + all pictures imported

--- login.php
+++ login.php
@@ -1,13 +1,19 @@
 <?php /* Template Name: Login Page */ ?>
 
 <head>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/login-style.css'?>">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/signup-style.css'?>">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/login-style.css?'?>">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/signup-style.css?'?>">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="<?php echo get_template_directory_uri() . '/js/login.js'?>"></script>
 </head>
 
 <body>
+<div class="header-container">
+<div class="header-image">
+    <div id="logo-image"></div>
+    </div>
+</div>
+
 <div class="login-form">
     <div class="login-form-section">
             <input placeholder="Email" id="login-form-input-email" type="text">
--- profile.php
+++ profile.php
@@ -4,15 +4,27 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/profile-style.css'?>">
 </head>
 
-<body>
-     <div id="profile-container">
-        <div class="profile-subcontainer">
-            <div id="profile-picture"></div>
-            <div id="profile-data"></div> 
+<header>
+    <div class="header-container">
+        <div class="header-image">
+            <div id="logo-image"></div>
         </div>
-        <div class="profile-subcontainer subcontainer2">
-            <div id="profile-description"></div>
-            <div id="profile-files"></div>
+    </div>
+</header>
+
+<div id="nav"></div>
+
+<body>
+    <div id="data-wrapper">
+        <div id="profile-container">
+            <div class="profile-subcontainer">
+                <div id="profile-picture"></div>
+                <div id="profile-data"></div> 
+            </div>
+            <div class="profile-subcontainer subcontainer2">
+                <div id="profile-description"></div>
+                <div id="profile-files"></div>
+            </div>
         </div>
     </div>
 </body>
