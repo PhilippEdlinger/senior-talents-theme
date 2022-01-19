c44d0d2d7022176310a8cffa0fc9a90d902d6a86
Philipp Edlinger <p.edlinger@students.htl-leonding.ac.at>
2022-01-19 09:40:06 +0100

    relocated link

--- profile_c.php
+++ profile_c.php
@@ -8,7 +8,6 @@
 
 <head>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/profile_c-style.css'?>">
-    <script src="<?php echo get_template_directory_uri() . '/js/profile_c.js'?>"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> 
 </head>
@@ -173,3 +172,5 @@
         </div>
     </div>
 </body>
+
+<script src="<?php echo get_template_directory_uri() . '/js/profile_c.js'?>"></script>
