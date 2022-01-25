3aa3dc9bbc9c917985930393b4ab7038f87cc005
Philipp Edlinger <p.edlinger@students.htl-leonding.ac.at>
2022-01-25 16:08:32 +0100

    added job market place

--- marketplace.php
+++ marketplace.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Profile Page */ ?>
+<?php /* Template Name: marketplace page*/ ?>
 
 <head>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/marketplace-style.css?'?>">
@@ -6,5 +6,24 @@
 </head>
 
 <body>
-    <div id="jobs-offer-area"></div>
+    <div id="jobs-offer-area">
+    <?php 
+
+        $request = wp_remote_get( 'http://localhost:8080/job-offer/getRandomJobOffers/1' . $_SESSION["companyId"], $arg );
+        $body    = wp_remote_retrieve_body($request); 
+        $jobList = json_decode($body, true);
+
+        if ($jobList != null){
+        foreach ($jobList as $job){
+        echo '<div class="joboffer">
+                <h3  class="joboffer-title">' . $job["title"] . '</h3>
+                <p name="joboffer-category" class="joboffer-category">Kategory: ' . $job["category"] . '</p>
+                <p class="joboffer-descr">Kurzbeschreibung: '. $job["descr"] .'</p>
+                <p>Kennnummer: '. $job["jobOfferId"] .'</p>
+               </div>';
+            }
+        }
+        ?>
+
+    </div>
 </body>
